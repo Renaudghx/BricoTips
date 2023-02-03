59e6713f86fbb9a432bdc75743d035f7eb48163c
renaudghx <renaudgheux@gmail.com>
2023-02-03 15:29:06 +0100

    Ajout Widget + shortcode

--- wp-content/themes/bricotips/footer.php
+++ wp-content/themes/bricotips/footer.php
@@ -1,8 +1,22 @@
-        <footer id="site-footer" class="header-footer-group">
-              ©BricoTips depuis 2022
-        </footer><!-- #site-footer -->
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
 
-      <?php wp_footer(); ?>
+?>
+			<footer id="site-footer" class="header-footer-group">
+                ©BricoTips depuis 2022
+			</footer><!-- #site-footer -->
 
-   </body>
+		<?php wp_footer(); ?>
+
+	</body>
 </html>
